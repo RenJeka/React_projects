e30ff704875d6d45224f418b5637c63608133528
renjeka <renjeka@gmail.com>
2023-05-04 17:50:51 +0300

    Feature: HW 4 task-4 (modified DidUpdateCounter)

--- CBS/HW/src/components/lesson4/DidUpdateCounter/DidUpdateCounter.component.tsx
+++ CBS/HW/src/components/lesson4/DidUpdateCounter/DidUpdateCounter.component.tsx
@@ -16,9 +16,15 @@ class DidUpdateCounterComponent extends Component<Props, State> {
 
     private didUpdateCounter = 0;
 
+    shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<State>, nextContext: any): boolean {
+        console.log('shouldComponentUpdate. nextState:', nextState);
+
+        return this.didUpdateCounter < 4;
+    }
+
     componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any): void {
         this.didUpdateCounter = this.state.updateCounter;
-        alert(`Component Did update: ${this.state.updateCounter}`);
+        alert(`Component Did update. "didUpdateCounter": ${this.didUpdateCounter}`);
     }
 
     private trigUpdate() {
