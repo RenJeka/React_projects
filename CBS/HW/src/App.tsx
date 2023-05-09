d45cac5e4e328dd77742388d3caf3259e7371907
renjeka <renjeka@gmail.com>
2023-05-09 18:03:31 +0300

    feature: HW-6 (ClassWork-1) complete Async Todos

--- CBS/HW/src/App.tsx
+++ CBS/HW/src/App.tsx
@@ -3,22 +3,15 @@ import './assets/styles/pico.min.css';
 import './App.scss';
 import {PageComponent} from "./barrel";
 import {connect} from "react-redux";
-import {changeTitle} from "./components/Title/action";
-import {MyStore} from "./redux/initialStore";
 
-interface Props {
-    dispatch: any;
-    // children?: JSX.Element|JSX.Element[];
+class Props {
 }
-
 
 class App extends React.Component<Props> {
     render() {
-        const {dispatch} = this.props;
         return (
             <div className="App">
                 <PageComponent/>
-                {/*<button onClick={() => dispatch(changeTitle("Change title from APP"))}>Change title from APP</button>*/}
             </div>
         );
     }
