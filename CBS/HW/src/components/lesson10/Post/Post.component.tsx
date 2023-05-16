9474b86a1b8a47ed146e173ef404409e2c26f29a
renjeka <renjeka@gmail.com>
2023-05-16 20:02:48 +0300

    feature: ClassWork-10 (Posts)  • End post component

--- CBS/HW/src/components/lesson10/Post/Post.component.tsx
+++ CBS/HW/src/components/lesson10/Post/Post.component.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router';
+import axios, {AxiosResponse} from "axios";
+import {Post} from "../../../barrel";
 
 const PostComponent = () => {
 
     const {postId} = useParams();
 
+    const [post, setPost] = useState<Post>();
+    useEffect(() => {
+        axios(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then((response: AxiosResponse<Post>) => {
+                setPost(response.data);
+            });
+    }, [postId]);
     return (
         <div>
-            POST # {postId}
+            POST: {post?.title}
         </div>
     );
 };
